c5bdf725b155eabe66b3f26b740bc39d0b6b40da
unknown <gabrielffguimaraes@gmail.com>
2022-05-09 16:06:02 -0300

    adding category form

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -4,15 +4,17 @@ import { CommonModule } from '@angular/common';
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoriesListComponent } from './categories-list/categories-list.component';
 import { CategoryFormComponent } from './category-form/category-form.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     CategoriesListComponent,
     CategoryFormComponent
   ],
-  imports: [
-    CommonModule,
-    CategoriesRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        CategoriesRoutingModule,
+        ReactiveFormsModule
+    ]
 })
 export class CategoriesModule { }
--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup , Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-category-form',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category-form.component.css']
 })
 export class CategoryFormComponent implements OnInit {
-
+  formGroup!:FormGroup;
   constructor() { }
 
   ngOnInit(): void {
+    this.formGroup = new FormBuilder().group({
+      name:[null,Validators.min(2)],
+      description:[null]
+    })
   }
 
 }
