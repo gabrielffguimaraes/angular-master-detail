5907efe2ca8784d94b68f4da5f930180074ecfe7
unknown <gabrielffguimaraes@gmail.com>
2022-05-09 17:45:41 -0300

    improving category form

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -1,20 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
 import {FormBuilder, FormGroup , Validators} from "@angular/forms";
+import {Category} from "../shared/category.model";
+import {CategoryService} from "../shared/category.service";
+import {ActivatedRoute, Route, Router} from "@angular/router";
+import {map, switchMap} from "rxjs/operators";
+import {EMPTY, of} from "rxjs";
 
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
   styleUrls: ['./category-form.component.css']
 })
-export class CategoryFormComponent implements OnInit {
+export class CategoryFormComponent implements OnInit,AfterContentChecked {
   formGroup!:FormGroup;
-  constructor() { }
+  currentAction!: string;
+  pageTitle!: string;
+  serverErroMessages!: string[];
+  submittingForm: boolean = false;
+  category:Category = new Category();
+
+  constructor(private categoryService:CategoryService,private route:ActivatedRoute,private router:Router,) { }
 
   ngOnInit(): void {
+    /*FACTORY*/
+    this.setCurrentAction();
+    this.buildCategoryForm();
+    this.loadCategory();
     this.formGroup = new FormBuilder().group({
       name:[null,Validators.min(2)],
       description:[null]
     })
   }
+  // PRIVATE
+  private setCurrentAction():void {
+    // @ts-ignore
+    this.route.paramMap
+      .pipe(
+        switchMap(params => (params.has('id')) ? this.categoryService.getById(parseInt(<string>params.get('id'))) : EMPTY),
+      ).subscribe((category:Category) => {
+          console.log(category);
+      }, (error) => {
+          console.log(error);
+      }, () => {
 
+      });
+  }
+  private buildCategoryForm():void {
+
+  }
+  private loadCategory():void {
+
+  }
+
+  ngAfterContentChecked(): void {
+  }
 }
--- src/app/pages/categories/shared/category.model.ts
+++ src/app/pages/categories/shared/category.model.ts
@@ -1,6 +1,6 @@
 export class Category {
-  constructor(public id:number,
-              public name:string,
-              public description:string) {
-  }
+  public id!:number;
+  public name!:string;
+  public description!:string
+  constructor() {}
 }
